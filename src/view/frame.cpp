bfe7c1b6ace23bdd5c59b8e2cb25fb430166ebef
John Petersen <sobeita@gmail.com>
2017-01-27 19:40:05 -0500

    Added some event handling

--- src/view/frame.cpp
+++ src/view/frame.cpp
@@ -9,15 +9,26 @@ namespace View {
 	bool Frame::run_once(void)
 	{
 		SDL_Event ev;
-		if(SDL_PollEvent(&ev)) {
+		while(SDL_PollEvent(&ev)) {
 			switch(ev.type) {
+				case SDL_WINDOWEVENT_CLOSE: {
+					return false;
+				}
+				case SDL_QUIT:
 				case SDL_WINDOWEVENT: {
-					auto &window = ev.window;
-					/*if(window = win) {
-					}*/
+					//auto &window = ev.window;
 				} break;
+				case SDL_KEYDOWN: {
+					auto &key = ev.key;
+					switch(key.keysym.sym) {
+						case SDLK_ESCAPE:
+							return false; break;
+					}
+				} break;
+				default: break;
 			}
 		}
+		SDL_Delay(200);
 		return true;
 	}
 	bool Frame::run(void)
