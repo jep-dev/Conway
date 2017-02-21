c5f1316a267fcbaacda2ee10c13e3bcb9c31c821
John Petersen <sobeita@gmail.com>
2017-02-21 08:49:52 -0500

    Replaced nullary constructor with default arg to unary constructor

--- include/view/frame.hpp
+++ include/view/frame.hpp
@@ -17,8 +17,9 @@ namespace View {
  		 * @param tn Arguments to forward to SDL_CreateWindow. */
 		/*template<typename... TN> Frame(TN... tn):
 			win(SDL_CreateWindow(tn...)) {}*/
-		Frame(void);
-		Frame(SDL_Window*);
+		/*Frame(void);
+		Frame(SDL_Window*);*/
+		Frame(SDL_Window * = SDL_CreateWindow("", 0, 0, 640, 480, 0));
 		virtual ~Frame(void);
 		//Frame(SDL_Window* win): win(win) {}
 		/** Destructor; frees the child window. */
