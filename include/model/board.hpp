3e8eb331d69c58e31e948d7a6fa8f8d8f41cf1ca
John Petersen <sobeita@gmail.com>
2017-04-05 07:31:48 -0400

    Added declarations for basic cell toggle/query

--- include/model/board.hpp
+++ include/model/board.hpp
@@ -2,9 +2,22 @@
 #define BOARD_HPP
 
 namespace Model {
+	// TODO choose data structure for cell storage
 	struct Board {
+		/** Toggle the state of a single cell.
+		  @param x X coordinate
+		  @param y Y coordinate */
+		void toggle(int x, int y);
+		/** Check the state of a single cell.
+		  @param x X coordinate
+		  @param y Y coordinate
+		  @return True if and only if the cell is alive */
+		bool query(int x, int y);
+		/** Constructor */
 		Board(void);
+		/** Destructor */
 		virtual ~Board(void);
 	};
 }
+
 #endif
