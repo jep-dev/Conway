b7136aec8caa829e91c5ff5e348b6aedd12ab8e4
John Petersen <sobeita@gmail.com>
2017-04-05 07:32:03 -0400

    Removed unused

--- include/view/frame.hpp
+++ include/view/frame.hpp
@@ -9,21 +9,17 @@ namespace View {
 		/** Polls and processes one event.
  		 * @return true unless the frame should close. */
 		bool poll(void);
+
 		/** Polls until it returns false.
  		 * @return true unless the frame should close. */
 		bool run(void);
 
 		/** Constructor; forwards the arguments to SDL_CreateWindow.
  		 * @param tn Arguments to forward to SDL_CreateWindow. */
-		/*template<typename... TN> Frame(TN... tn):
-			win(SDL_CreateWindow(tn...)) {}*/
-		/*Frame(void);
-		Frame(SDL_Window*);*/
 		Frame(SDL_Window * = SDL_CreateWindow("", 0, 0, 640, 480, 0));
-		virtual ~Frame(void);
-		//Frame(SDL_Window* win): win(win) {}
+
 		/** Destructor; frees the child window. */
-		//virtual ~Frame(void) = default;
+		virtual ~Frame(void);
 	private:
 		SDL_Window *win;
 	};
