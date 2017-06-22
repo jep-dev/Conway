63ea463e503b736537fb95c1b74e51d43fcc08b0
John Petersen <sobeita@gmail.com>
2017-06-22 19:28:01 -0400

    Minor changes to frame, mostly formatting

--- include/view/frame.hpp
+++ include/view/frame.hpp
@@ -9,14 +9,17 @@ namespace View {
 		/** Polls and processes one event.
  		 * @return true unless the frame should close. */
 		bool poll(void);
+		//bool poll(void) { return false; }
 
 		/** Polls until it returns false.
  		 * @return true unless the frame should close. */
 		bool run(void);
+		//bool run(void) { return false; }
 
 		/** Constructor; forwards the arguments to SDL_CreateWindow.
  		 * @param tn Arguments to forward to SDL_CreateWindow. */
-		Frame(SDL_Window * = SDL_CreateWindow("", 0, 0, 640, 480, 0));
+		Frame(SDL_Window *win = SDL_CreateWindow("", 0, 0, 640, 480, 0));
+			/*win(win) {} */
 
 		/** Destructor; frees the child window. */
 		virtual ~Frame(void);
--- src/view/frame.cpp
+++ src/view/frame.cpp
@@ -1,44 +1,50 @@
-#include "frame.hpp"
+#include "view/frame.hpp"
 
 
 namespace View {
-	Frame::Frame(SDL_Window *win): win(win) {}
+
+	Frame::Frame(SDL_Window *win):
+		win(win) {}
+
 	Frame::~Frame(void)
 	{
 		if(win) SDL_DestroyWindow(win);
 	}
+
 	bool Frame::poll(void) {
 		SDL_Event ev;
 		while(SDL_PollEvent(&ev)) {
 			switch(ev.type) {
-				case SDL_WINDOWEVENT_CLOSE: {
+				case SDL_WINDOWEVENT_CLOSE:
 					return false;
-				}
-				case SDL_QUIT: {
+				case SDL_QUIT:
 					return false;
-				}
-				case SDL_WINDOWEVENT: {
-					if(ev.window.windowID == SDL_GetWindowID(win)) {
-						switch(ev.window.type) {
-							case SDL_WINDOWEVENT_CLOSE:
-								return false;
-							case SDL_WINDOWEVENT_RESIZED:
-								// TODO
-								break;
-							case SDL_KEYDOWN:
-								return false;
-							default:
-								break;
+				case SDL_WINDOWEVENT:
+					{
+						if(ev.window.windowID == SDL_GetWindowID(win)) {
+							switch(ev.window.type) {
+								case SDL_WINDOWEVENT_CLOSE:
+									return false;
+								case SDL_WINDOWEVENT_RESIZED:
+									// TODO
+									break;
+								case SDL_KEYDOWN:
+									return false;
+								default:
+									break;
+							}
 						}
 					}
-				} break;
-				case SDL_KEYDOWN: {
-					auto &key = ev.key;
-					switch(key.keysym.sym) {
-						case SDLK_ESCAPE:
-							return false;
+					break;
+				case SDL_KEYDOWN:
+					{
+						auto &key = ev.key;
+						switch(key.keysym.sym) {
+							case SDLK_ESCAPE:
+								return false;
+						}
 					}
-				} break;
+					break;
 				default: break;
 			}
 		}
