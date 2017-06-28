c27b7b016d5aac65d180455502cd86bf2a19bb1e
John Petersen <sobeita@gmail.com>
2017-06-28 08:47:40 -0400

    Moved frame during restructuring

---
+++ include/view_frame.hpp
@@ -0,0 +1,30 @@
+#ifndef VIEW_FRAME_HPP
+#define VIEW_FRAME_HPP
+
+#include "SDL2/SDL.h"
+#include "runnable.hpp"
+
+namespace View {
+
+	struct Frame : public Runnable {
+		/** Polls and processes one event.
+ 		 * @return true unless the frame should close. */
+		virtual bool poll(void);
+
+		/** Polls until it returns false.
+ 		 * @return true unless the frame should close. */
+		virtual bool run(void);
+
+		/** Constructor; forwards the arguments to SDL_CreateWindow.
+ 		 * @param tn Arguments to forward to SDL_CreateWindow. */
+		Frame(SDL_Window *win = SDL_CreateWindow("", 0, 0, 640, 480, 0));
+
+		/** Destructor; frees the child window. */
+		virtual ~Frame(void);
+	private:
+		SDL_Window *win;
+	};
+
+}
+
+#endif
