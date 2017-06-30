8cae4b1738c61fff6c96dc94bb8b94f9ed3e9f68
John Petersen <sobeita@gmail.com>
2017-06-30 08:50:37 -0400

    Added EVID as shorthand for decltype(SDL_Event::type); added event handler template

--- include/view_frame.hpp
+++ include/view_frame.hpp
@@ -3,10 +3,13 @@
 
 #include "SDL2/SDL.h"
 #include "runnable.hpp"
+#include "templates.hpp"
 
 namespace View {
 
+	typedef decltype(SDL_Event::type) EVID;
 	struct Frame : public Runnable {
+
 		/** Polls and processes one event.
  		 * @return true unless the frame should close. */
 		virtual bool poll(void);
@@ -14,6 +17,16 @@ namespace View {
 		/** Polls until it returns false.
  		 * @return true unless the frame should close. */
 		virtual bool run(void);
+
+		/** Template for event handlers; signature will change soon.
+		 * @tparam S type of non-optional first argument, soon SDL_Event.
+		 * @tparam T type(s) of optional argument(s).
+		 * @param evid event id from SDL_Event::type.
+		 * @param s non-optional first argument, soon an SDL_Event.
+		 * @param t optional argument(s), currently used for message passing.
+		 * @return true unless the frame should close. */
+		template<typename S, typename... T>
+		bool handle(EVID && evid, S && s, T &&... t);
 
 		/** Constructor; forwards the arguments to SDL_CreateWindow.
  		 * @param win The window managed by the Frame */
