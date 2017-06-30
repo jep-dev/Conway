a67d85f2f40f93655698c3c7c35ca79caa0b9803
John Petersen <sobeita@gmail.com>
2017-06-30 15:57:43 -0400

    Testing GPG key

--- include/modular_main.hpp
+++ include/modular_main.hpp
@@ -5,5 +5,10 @@
 #include "view_panel.hpp"
 #include "runnable.hpp"
 
+/** Entry point for the program
+  @param argc Number of arguments.
+  @param argv Array of arguments.
+  @return 0 on success, 1 otherwise. */
+int main(int argc, const char *argv[]);
 
 #endif
