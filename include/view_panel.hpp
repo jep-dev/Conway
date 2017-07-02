41da63af623680cf60c095f178424760c70c1ee4
John Petersen <jepetersen.dev@gmail.com>
2017-07-02 15:53:00 -0400

    Updated documentation with @brief

--- include/view_panel.hpp
+++ include/view_panel.hpp
@@ -4,10 +4,13 @@
 #include "runnable.hpp"
 
 namespace View {
+	/** @brief A partition of a canvas */
 	struct Panel : public Runnable {
 		virtual bool poll(void);
 		virtual bool run(void);
+		/** @brief Constructor */
 		Panel(void);
+		/** @brief Descructor */
 		virtual ~Panel(void);
 	};
 }
