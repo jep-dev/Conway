a5d55c4d5a3536300eb950f1741bb6f8f20f35f6
John Petersen <jepetersen.dev@gmail.com>
2017-07-12 00:30:44 -0400

    Added type information for template troubleshooting with ABI and fallback; functional for both but could use revision (STL accumulate? Recursion?)

--- include/templates.hpp
+++ include/templates.hpp
@@ -2,7 +2,42 @@
 #define TEMPLATES_HPP
 
 #include <type_traits>
-#include <functional>
+#include <typeinfo>
+#include <string>
+
+/* Type introspection for template troubleshooting */
+#ifdef __GNUG__
+#include <cstdlib>
+#include <cxxabi.h>
+#include <memory>
+
+/* Use abi when available to get legible names. Consider wrapping these in
+   functions or lambdas for portability, instead of multiple statements
+   hidden behind a macro. */
+#define DEMANGLE(X, Y) \
+	int status = 0; \
+	std::unique_ptr<char, void (*)(void*)> demangled \
+	{ abi::__cxa_demangle(Y, NULL, NULL, &status), std::free }; \
+		X << Y << " / " << (status ? Y : demangled.get())
+#else
+/*  Otherwise default to get mangled names (e.g. std::cout -> So).
+	Try post hoc object file introspection using nm or equivalent if this
+	implementation is chosen by your compiler. */
+#define DEMANGLE(X, Y) X << Y
+#endif
+
+/** @brief Print (stream insert) the names of the given types.
+  @tparam T The sequence of types to print.
+  @tparam S The type of the destination stream.
+  @param s The destination (potentially standard output or a log file).
+  @return The destination stream after insertion. */
+template<typename... T, typename S>
+S& get_types (S &s) {
+	for(auto && name : {typeid(T).name()...}) {
+		DEMANGLE(s, name) << "; ";
+	}
+	return s;
+}
 
 /** @brief Defines value as true if the given type's value is equivalent to true.
   @tparam T The given type with value typedef convertible to boolean. */
@@ -93,6 +128,5 @@ struct none_match_t:
 	none_match_tt<std::is_same,
 		std::integral_constant<T, T1>,
 		std::integral_constant<T, TN>...> {};
-
 
 #endif
