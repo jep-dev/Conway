d3bb41a5c4eb5bdeb4361035ed8c44a6823bba4a
John Petersen <jepetersen.dev@gmail.com>
2017-07-12 00:42:00 -0400

    Removed assertions from templates.hpp, event handler dispatch is done by simple template argument deduction

--- src/modular_main.cpp
+++ src/modular_main.cpp
@@ -1,35 +1,9 @@
 #include "modular_main.hpp"
 
-// #include <iostream>
-
 int main(int argc, const char *argv[])
 {
 	if(SDL_Init(SDL_INIT_VIDEO)) return 1;
 	View::Frame f;
 	f.run();
 	SDL_Quit();
-
-	typedef std::true_type TT;
-	typedef std::false_type FT;
-
-	static_assert(is_true<TT>::value, "identity of true");
-	static_assert(!is_true<FT>::value, "identity of false");
-	static_assert(none_true<bool,0,0>::value, "none of {0,0}");
-	static_assert(!none_true<bool,0,1>::value, "any of {0,1}");
-	static_assert(!none_true<bool,1,0>::value, "any of {1,0}");
-	static_assert(!none_true<bool,1,1>::value, "any of {1,1}");
-	static_assert(!none_true<bool,0,0,0,1>::value, "any of {0, 0..., 1}");
-
-	static_assert(!none_same_t<int, bool, int>::value,
-			"int one of {bool, int}");
-	static_assert(none_same_t<int, bool, char>::value,
-			"int none of {bool, char}");
-
-	static_assert(none_match_t<View::EVID,
-			SDL_WINDOWEVENT_CLOSE, SDL_QUIT, SDL_KEYDOWN>::value,
-			"Close EVID none of {Quit, Keydown}");
-	static_assert(!none_match_t<View::EVID, SDL_WINDOWEVENT_CLOSE,
-			SDL_WINDOWEVENT_ENTER, SDL_WINDOWEVENT_CLOSE>::value,
-			"Close EVID one of {Enter, Close}");
-
 }
