e106e6fba635fe2b7b72369e344913406b1d883c
John Petersen <jepetersen.dev@gmail.com>
2017-07-24 19:01:47 -0400

    Removed Runnable section (may need later)

--- src/panel.cpp
+++ src/panel.cpp
@@ -2,18 +2,9 @@
 
 namespace View {
 	Panel::Panel(void): TaskBase() {}
-} /*
-namespace Runnable {
-	template<>
-	e_task TaskBase<View::Panel>::call(e_task e) {
-		return e_task::run;
-	}
-}
-namespace View { */
 	template<>
 	e_task Panel::call(Runnable::e_task e) {
 		// TODO stub
 		return e;
 	}
-
 }
