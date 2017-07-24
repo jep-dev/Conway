c08ffc9f7b66a5dfe4256b8213bc7e38d2a2d96d
John Petersen <jepetersen.dev@gmail.com>
2017-07-24 18:10:43 -0400

    Moved 'using' and struct declarations into 'view'

--- include/view.hpp
+++ include/view.hpp
@@ -12,13 +12,17 @@
 #include "SDL2/SDL.h"
 
 // Internal
-#include "templates.hpp"
-
 #include "runnable.hpp"
 #include "driver.hpp"
 
 namespace View {
+	using Runnable::e_task;
+	using Runnable::TaskBase;
+	using Runnable::Driver;
+
 	typedef decltype(SDL_Event::type) EVID;
+	struct Frame;
+	struct Panel;
 }
 
 #include "frame.hpp"
