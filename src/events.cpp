f9c23aad02b0b96445bd04a6bf9b125e53fae92a
John Petersen <jepetersen.dev@gmail.com>
2017-07-25 00:03:46 -0400

    Moved 'events' to 'handler'

--- src/events.cpp
+++
@@ -1,8 +0,0 @@
-#include "view.hpp"
-
-namespace View {
-	Runnable::e_task Handler::call(Runnable::e_task e, SDL_Event &ev) {
-		return e_task::quit;
-	}
-	Handler::Handler(void): Runnable::TaskBase<Handler, SDL_Event>() {}
-}
