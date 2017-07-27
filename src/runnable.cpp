65806fe9a7b11e8b8bae3580b0f120c37062cf6a
John Petersen <jepetersen.dev@gmail.com>
2017-07-27 11:45:53 -0400

    Moved implementation to cpp from tpp

--- src/runnable.cpp
+++ src/runnable.cpp
@@ -1,8 +1,23 @@
 #include "runnable.hpp"
 
 namespace Runnable {
-	std::ostream& operator<<(std::ostream & os, e_task const& e) {
-		switch(e) {
+	std::ostream& operator<<(std::ostream & os, Signal const& s) {
+		auto matches = 0;
+		for(auto it : {Signal::Quit, Signal::Fail, Signal::Pass,
+				Signal::Yield, Signal::Start}) {
+			if(s & it) {
+				matches++;
+				if(matches > 1) os << ',';
+				switch(s) {
+					case Quit: os << "Quit";
+					case Fail: os << "Fail";
+					case Pass: os << "Pass";
+					case Yield: os << "Yield";
+					case Start: os << "Start";
+				}
+			}
+		}
+		/* switch(e) {
 			case e_task::start: return os << "start";
 			case e_task::pause: return os << "pause";
 			case e_task::run:   return os << "run";
@@ -10,6 +25,6 @@ namespace Runnable {
 			case e_task::quit:  return os << "quit";
 			case e_task::pass:  return os << "pass";
 			default:            return os << "unknown";
-		}
+		} */
 	}
 }
