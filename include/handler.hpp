164ef3e9bc3fed633e00bfafe2a11bfcd9594190
John Petersen <jepetersen.dev@gmail.com>
2017-07-27 01:37:39 -0400

    Rewrote handlers

--- include/handler.hpp
+++ include/handler.hpp
@@ -1,22 +1,64 @@
-#ifndef VIEW_EVENTS_HPP
-#define VIEW_EVENTS_HPP
+#ifndef RUNNABLE_EVENTS_HPP
+#define RUNNABLE_EVENTS_HPP
 
-#include "view.hpp"
+#include <tuple>
 
-namespace View {
-	/* template<unsigned MAJOR>
-	struct Handler: Runnable::TaskBase<Handler<MAJOR>, const SDL_Event> { */
-	template<SDL_EventType E>
-	struct Handler: Runnable::TaskBase<Handler<E>, const SDL_Event> {
-		Runnable::e_task call(Runnable::e_task t, SDL_Event const& ev);
-		Handler(void);
-		virtual ~Handler(void) = default;
+#include "runnable.hpp"
+#include "SDL2/SDL.h"
+
+namespace Runnable {
+	/* Handler with compile-time emulated virtual methods */
+	template<typename D, typename SUBJ, typename... OBJS>
+	struct HandlerCRTP;
+	/* Specialization of HandlerCRTP that prepends const SDL_Event to OBJS */
+	template<bool EXACT, uint32_t ID, typename D, typename SUBJ, typename... OBJS>
+	struct EventHandlerCRTP;
+	/* Collection of types by invariance over subject and objects */
+	template<typename SUBJ, typename... OBJS>
+	struct EventHandlers;
+
+	template<typename D, typename SUBJ, typename... OBJS>
+	struct HandlerCRTP {
+		e_task handle(e_task task, SUBJ & subj, OBJS &... objs) {
+			return static_cast<D*>(this) -> handle(task, subj, objs...);
+		}
 	};
-	struct HandlerBase: Runnable::TaskBase<HandlerBase, const SDL_Event> {
-		Runnable::e_task call(Runnable::e_task e, SDL_Event const& ev);
-		HandlerBase(void);
-		virtual ~HandlerBase(void) = default;
+	template<bool EXACT, uint32_t ID, typename D, typename SUBJ, typename... OBJS>
+	struct EventHandlerCRTP: HandlerCRTP<EventHandlerCRTP<EXACT, ID, D, SUBJ, OBJS...>,
+			const SDL_Event, SUBJ, OBJS...> {
+		e_task handle(e_task task, SUBJ &subj, SDL_Event const& ev, OBJS &... objs) {
+			return static_cast<D*>(this) -> handle(task, subj, ev, objs...);
+		}
 	};
+	template<typename SUBJ, typename... OBJS>
+	struct EventHandlers {
+		template<bool EXACT, uint32_t ID, typename D>
+		struct EventHandler;
+
+		template<bool EXACT, uint32_t ID, typename D>
+		struct EventHandler: EventHandlerCRTP<EXACT, ID, EventHandlers<SUBJ, OBJS...>
+				::EventHandler<EXACT, ID, D>, SUBJ, OBJS...> {
+			e_task handle(e_task task, SUBJ &subj, OBJS &... objs, SDL_Event const& ev);
+		};
+	private:
+		EventHandlers(void);
+	};
+
+	template<bool EXACT, typename D, typename SUBJ, typename... OBJS>
+	using QuitEventHandler = typename EventHandlers<SUBJ, const SDL_Event, OBJS...>
+		::template EventHandler<EXACT, SDL_QUIT, D>;
+	template<bool EXACT, typename D, typename SUBJ, typename... OBJS>
+	using WindowEventHandler = typename EventHandlers<SUBJ, const SDL_Event, OBJS...>
+		::template EventHandler<EXACT, SDL_WINDOWEVENT, D>;
+	template<bool EXACT, typename D, typename SUBJ, typename... OBJS>
+	using KeyEventHandler = typename EventHandlers<SUBJ, const SDL_Event, OBJS...>
+		::template EventHandler<EXACT, SDL_KEYDOWN, D>;
+	template<bool EXACT, typename D, typename SUBJ, typename... OBJS>
+	using MouseEventHandler = typename EventHandlers<SUBJ, const SDL_Event, OBJS...>
+		::template EventHandler<EXACT, SDL_MOUSEMOTION, D>;
+	template<bool EXACT, typename D, typename SUBJ, typename... OBJS>
+	using UserEventHandler = typename EventHandlers<SUBJ, const SDL_Event, OBJS...>
+		::template EventHandler<EXACT, SDL_USEREVENT, D>;
 }
 
 #endif
--- src/handler.cpp
+++ src/handler.cpp
@@ -1,48 +1,13 @@
-#include "view.hpp"
+#include "handler.hpp"
 
-namespace View {
-	/* template<unsigned MAJOR> Runnable::e_task
-	Handler<MAJOR>::call(Runnable::e_task e, SDL_Event const& ev) {
-		switch(e) {
-		}
-	} */
-	template<> e_task
-	Handler<SDL_QUIT>::call(e_task t, SDL_Event const& ev) {
-		return e_task::quit; // TODO
+namespace Runnable {
+	/*template<typename D, typename... HN>
+	e_task Handler<D,HN...>::call(
+		e_task t, D &d, SDL_Event const& ev, HN &... hn)
+	{
+		return e_task::quit;
 	}
-	template<> e_task
-	Handler<SDL_WINDOWEVENT>::call(e_task t, SDL_Event const& ev) {
-		// TODO view -> handler
-		return ev.window.type == SDL_WINDOWEVENT_CLOSE ?
-			e_task::quit : t;
-	}
-	template<> e_task
-	Handler<SDL_MOUSEMOTION>::call(e_task t, SDL_Event const& ev) {
-		// TODO handler -> model
-		return t;
-	}
-	template<> e_task
-	Handler<SDL_KEYDOWN>::call(e_task t, SDL_Event const& ev) {
-		return t;
-	}
-	template<SDL_EventType E>
-	Handler<E>::Handler(void):
-		Runnable::TaskBase<Handler<E>, const SDL_Event>() {}
-
-	Runnable::e_task
-	HandlerBase::call(Runnable::e_task t, SDL_Event const& ev) {
-		switch(ev.type & 0xff00) {
-			case SDL_WINDOWEVENT:
-				return Handler<SDL_WINDOWEVENT>().call(t, ev);
-			case SDL_QUIT:
-				return Handler<SDL_QUIT>().call(t, ev);
-			case SDL_MOUSEMOTION:
-				return Handler<SDL_MOUSEMOTION>().call(t, ev);
-			case SDL_KEYDOWN:
-				return Handler<SDL_KEYDOWN>().call(t, ev);
-			default: return t;
-		}
-	}
-	HandlerBase::HandlerBase(void):
-		Runnable::TaskBase<HandlerBase, const SDL_Event>() {}
+	template<typename D, typename... HN>
+	Handler<D,HN...>::Handler(void):
+		TaskBase<Handler<D,HN...>, D, const SDL_Event, HN...>() {}*/
 }
